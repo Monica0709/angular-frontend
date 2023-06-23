a65c0a7fb2551e1fd3003593818cf9ee9f2757bd
monica <monica.surendar@gmail.com>
2023-06-23 09:48:56 +0530

    Sample download

--- src/app/excell/excell.component.ts
+++ src/app/excell/excell.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as XLSX from 'xlsx';
+
 @Component({
   selector: 'app-excell',
   templateUrl: './excell.component.html',
@@ -10,5 +12,18 @@ export class ExcellComponent implements OnInit{
     
   }
   constructor( private router: Router){}
+  downloadSampleExcel(): void {
+    const data: Array<any> = [
+      ['Name', 'Age', 'City'],
+      ['John Doe', 30, 'New York'],
+      ['Jane Smith', 25, 'London'],
+      ['Bob Johnson', 40, 'Paris']
+    ];
 
+    const worksheet: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet(data);
+    const workbook: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+
+    XLSX.writeFile(workbook, 'sample.xlsx');
+  }
 }
